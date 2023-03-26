f7dd6d2180aeff534dc9fcc8aa43ec081d7b21f8
SK-LuG-AG <113166636+SK-LuG-AG@users.noreply.github.com>
2023-03-26 12:52:59 +0200

    Add files via upload

---
+++ Cars.cs
@@ -0,0 +1,11 @@
+﻿namespace CarsApp
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Hersteller { get; set; } = string.Empty;
+        public string Herkunftsland { get; set; } = string.Empty;  
+        public int Baujahr { get; set; }
+    }
+}
